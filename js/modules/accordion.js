e27602d5681fb4ceeda41a99b1c70df5791c9d1c
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-02-28 19:49:53 -0300

    Accordion transformado em classe, e adicionado comentarios

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,29 @@
 //no conteudo FAQ mostra e esconde o conteúdo
-export default function initFaqAnimation() {
-  const hideList = document.querySelectorAll("[data-anime='accordion'] dt");
-  const activeClass = "ativo";
-  hideList[0].classList.add(activeClass);
-  hideList[0].nextElementSibling.classList.add(activeClass);
-  function ativar() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  //cria um objeto com accordionList referente a lista linkada
+  //e activeClass com a string 'ativo'
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
   }
-  if (hideList.length) {
-    hideList.forEach((item) => {
-      item.addEventListener("click", ativar);
+
+  //adiciona ou remove a classe 'ativo'
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+  //adiciona a funcao a cima para cada item da lista linkada
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+  //inicia a funcao se tiver alguma lista linkada
+  init() {
+    if (this.accordionList.length) {
+      //já deixa o primeiro item da lista ativo
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,28 +1,35 @@
 //Scroll suave ao clickar nos links interno
 export default class ScrollSuave {
   constructor(links, options) {
+    //pegas todos os links internos que vc selecionar
     this.linksInternosScroll = document.querySelectorAll(links);
+    //se n for colocado nada no segundo parametro por padrão
+    //o scroll vai ser { behavior: "smooth", block: "start" }
     if (options === undefined) {
       this.options = { behavior: "smooth", block: "start" };
     } else {
       this.options = options;
     }
+    //sempre o this vai fazer referencia ao this.scrollToSection
     this.scrollToSection = this.scrollToSection.bind(this);
   }
-
   scrollToSection(event) {
+    //previne o padrão
     event.preventDefault();
+    //seleciona o atributo do href do link clickado
     const href = event.currentTarget.getAttribute("href");
+    //seleciona a seção referente ao atributo referenciado pela ID
     const section = document.querySelector(href);
+    //adiciona o scroll suave com os valores de option
     section.scrollIntoView(this.options);
   }
-
+  //adiciona a funcao a cima para cada link
   addLinkEvent() {
     this.linksInternosScroll.forEach((link) => {
       link.addEventListener("click", this.scrollToSection);
     });
   }
-
+  //inicia a funcao se tiver algo linkado
   init() {
     if (this.linksInternosScroll.length) {
       this.addLinkEvent();
