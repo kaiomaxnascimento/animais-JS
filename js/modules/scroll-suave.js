93da7285f8c2c80268c10d981acdd141d469369d
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-02-28 17:43:31 -0300

    alterado o scroll suave de function para classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,19 +1,32 @@
 //Scroll suave ao clickar nos links interno
-export default function initScrollSuave() {
-  const linksInternosScroll = document.querySelectorAll(
-    "[data-menu='suave'] a[href^='#']",
-  );
-  function scrollToSection(event) {
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternosScroll = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternosScroll.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
-  linksInternosScroll.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+
+  init() {
+    if (this.linksInternosScroll.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
-initScrollSuave();
