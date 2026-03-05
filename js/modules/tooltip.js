7bb0b2622f1ddde21589c7c9442be079f1d7ebe9
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-04 21:11:30 -0300

    tooltip refatorada

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,45 +1,61 @@
 //tooltip ao passar o mouse em cima do mapa
-export default function initTooltip() {
-  //loop por todos os tooltip
-  const tooltip = document.querySelectorAll("[data-tooltip]");
-
-  tooltip.forEach((event) => {
-    event.addEventListener("mouseover", onMouseover);
-  });
-  //ao passar o mouse em cima do mapa chama o elemento criado
-  function onMouseover() {
-    const toolTip = criarTooltip(this);
-    onMouseMove.toolTip = toolTip;
-    //tirar o mouse do mapa remove o elemento
-    this.addEventListener("mouseleave", onMouseLeave);
-    onMouseLeave.element = this;
-    //elemento acompanha os eixos do mouse
-    onMouseLeave.toolTip = toolTip;
-    this.addEventListener("mousemove", onMouseMove);
+export default class Tooltip {
+  constructor(tooltip) {
+    //chamando a div que armazena o mapa
+    this.tooltip = document.querySelectorAll(tooltip);
+    //o this sempre vai fazer referência ao objeto definido
+    //pois perdem a referência qnd adiciona um evento
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
   }
-  //objeto criado para relacionar o evento de tirar o mouse do mapa
-  const onMouseLeave = {
-    //remover os eventos ao parar de usar os mesmos
-    handleEvent() {
-      this.toolTip.remove();
-      this.element.removeEventListener("mouseleave", onMouseLeave);
-      this.element.removeEventListener("mousemove", onMouseMove);
-    },
-  };
-  //objeto criado para relacionar o evento de mover o mouse em cima do mapa
-  const onMouseMove = {
-    handleEvent(event) {
-      this.toolTip.style.top = event.pageY + 20 + "px";
-      this.toolTip.style.left = event.pageX + 20 + "px";
-    },
-  };
-  //cria o elemento para aplicar o toolTip
-  function criarTooltip(element) {
+
+  //cria a div e atribui a ela o texto do aria-label
+  //e a classe tooltip e ao usar o this faz referencia a mesma.
+  criarTooltip(element) {
     const toolTipBox = document.createElement("div");
     const text = element.getAttribute("aria-label");
     toolTipBox.classList.add("tooltip");
     toolTipBox.innerText = text;
     document.body.appendChild(toolTipBox);
-    return toolTipBox;
+    this.toolTipBox = toolTipBox;
+  }
+
+  //faz a tooltip acompanhar o mouse
+  onMouseMove(event) {
+    this.toolTipBox.style.top = event.pageY + 20 + "px";
+    if (event.pageX + 240 > window.innerWidth) {
+      this.toolTipBox.style.left = event.pageX - 190 + "px";
+    } else {
+      this.toolTipBox.style.left = event.pageX + 20 + "px";
+    }
+  }
+
+  //remove os eventos ao retirar o mouse do mapa
+  onMouseLeave(event) {
+    this.toolTipBox.remove();
+    event.currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
+    event.currentTarget.removeEventListener("mousemove", this.onMouseMove);
+  }
+
+  //cria o elemento ao colocar o mouse em cima do mapa
+  //e remove ao retirar
+  onMouseOver(event) {
+    this.criarTooltip(event.currentTarget);
+    event.currentTarget.addEventListener("mouseleave", this.onMouseLeave);
+    event.currentTarget.addEventListener("mousemove", this.onMouseMove);
+  }
+
+  addTooltipsEvent() {
+    this.tooltip.forEach((event) => {
+      event.addEventListener("mouseover", this.onMouseOver);
+    });
+  }
+
+  init() {
+    if (this.tooltip.length) {
+      this.addTooltipsEvent();
+    }
+    return this;
   }
 }
