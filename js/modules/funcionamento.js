0a65e214dc102a25812c432f2012f85aa6832cad
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-07 20:26:53 -0300

    adicionado comentarios

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -5,11 +5,13 @@ export default class Funcionamento {
     this.activeClass = activeClass;
   }
 
+  //pega os valores do dataset e transformar em number
   dadosFuncionamento() {
     this.diaSemana = this.funcionamento.dataset.semana.split(",").map(Number);
     this.horaDia = this.funcionamento.dataset.horario.split(",").map(Number);
   }
 
+  //pega o dia e a hora de brasília
   dadosAgora() {
     this.dataAgora = new Date();
     this.diaAgora = this.dataAgora.getDay();
@@ -17,6 +19,7 @@ export default class Funcionamento {
   }
 
   estaAberto() {
+    //se o dia da semana é igual ao valor do dataset
     const diaAberto = this.diaSemana.indexOf(this.diaAgora);
     const horaAberto =
       this.horaAgora >= this.horaDia[0] && this.horaAgora < this.horaDia[1];
