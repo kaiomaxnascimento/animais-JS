8e91631e14fb5199c330d98d287fc0d628404b48
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-07 20:17:00 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBtc from "./modules/fetch-btc.js";
 import ScrollAnima from "./modules/scroll-animacao.js";
@@ -41,8 +41,8 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-initFuncionamento();
-
+const funcionamento = new Funcionamento("[data-semana]", 'aberto');
+funcionamento.init();
 
 fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBtc("https://blockchain.info/ticker", ".btc-preco");
